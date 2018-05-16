6f1b2fbd5849aa100a990ea04dd4281dabcf5a7f
Jonathan LaBroad <jon.labroad@gmail.com>
2018-05-16 17:48:15 -0400

    Fix

--- sensor/provision/provision.sh
+++ sensor/provision/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
-#sudo sh bootstrap-salt.sh
+wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
+sudo sh bootstrap-salt.sh
 
 cp -rf ./sensor/provision/etc/* /etc
